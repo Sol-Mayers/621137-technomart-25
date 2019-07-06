936f1eaed43c0b1897117e58fcfe2f2f11d42b54
Sol-Mayers <rinotello@yandex.ru>
2019-07-07 00:48:10 +0300

    Ред. 07.07.2019

---
+++ js/Popups.js
@@ -0,0 +1,36 @@
+// JavaScript Document
+function openForm() {
+    document.getElementById("popupWrite").style.display = "flex";
+}
+
+openForm();
+
+function closeForm() {
+    document.getElementById("popupWrite").style.display = "none";
+}
+
+closeForm();
+
+function openMap() {
+    document.getElementById("popupMap").style.display = "flex";
+}
+
+openMap();
+
+function closeMap() {
+    document.getElementById("popupMap").style.display = "none";
+}
+
+closeMap();
+
+function openCart() {
+    document.getElementById("popupCart").style.display = "flex";
+}
+
+openCart();
+
+function closeCart() {
+    document.getElementById("popupCart").style.display = "none";
+}
+
+closeCart();
